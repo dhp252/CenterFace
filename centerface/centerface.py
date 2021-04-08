268b7f30cda0281e77b5cb8fbbc16f57aa769f19
dhp252 <dhp252@gmail.com>
2021-04-09 00:24:52 +0700

    add cmt

--- centerface/centerface.py
+++ centerface/centerface.py
@@ -5,6 +5,29 @@ import datetime
 
 class CenterFace(object):
     def __init__(self, landmarks=True):
+        """
+        Example:
+        def camera():
+            cap = cv2.VideoCapture(0)
+            ret, frame = cap.read()
+            h, w = frame.shape[:2]
+            centerface = CenterFace(landmarks=True) # default
+            while True:
+                ret, frame = cap.read()
+                # dets, lms = centerface(frame, h, w, threshold=0.35) # centerface orignal
+                dets, lms = centerface(frame, threshold=0.35) # modified
+                for det in dets:
+                    boxes, score = det[:4], det[4]
+                    cv2.rectangle(frame, (int(boxes[0]), int(boxes[1])), (int(boxes[2]), int(boxes[3])), (2, 255, 0), 1)
+                for lm in lms:
+                    for i in range(0, 5):
+                        cv2.circle(frame, (int(lm[i * 2]), int(lm[i * 2 + 1])), 2, (0, 0, 255), -1)
+                cv2.imshow('out', frame)
+                # Press Q on keyboard to stop recording
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            cap.release()
+        """
         self.landmarks = landmarks
         if self.landmarks:
             self.net = cv2.dnn.readNetFromONNX('./centerface.onnx')
